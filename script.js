68fa448070b004a2e10d44636a8c6abb1d0e0279
Erez <erezfree29@gmail.com>
2021-07-05 12:57:51 -0700

    add onclick event

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const books = [];
+let books = [];
+const addBookButton = document.querySelector('button');
+addBookButton.addEventListener('click', () => {
+  const btitle = document.querySelector('.title').value;
+  const bauthor = document.querySelector('.author').value;
+  const book = {
+    title: btitle,
+    author: bauthor,
+  };
+  if (localStorage.getItem('savedArray') != null) {
+    books = JSON.parse(localStorage.getItem('savedArray'));
+  }
+  books.push(book);
+  localStorage.setItem('savedArray', JSON.stringify(books));
+  alert(books[1].title);
+})();
