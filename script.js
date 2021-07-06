daa9ba2b4e438cddfb6f9ecb209e6ea5357ecdfb
Erez <erezfree29@gmail.com>
2021-07-05 22:52:55 -0700

    remove button

--- script.js
+++ script.js
@@ -26,14 +26,33 @@ function showBooks() {
   for (let i = 0; i < books.length; i += 1) {
     const btitle = document.createElement('h5');
     btitle.className = 'bookname';
+    btitle.id = books[i].title;
     btitle.textContent = books[i].title;
     const bauthor = document.createElement('p');
     bauthor.className = 'bookauthor';
     bauthor.textContent = books[i].author;
-
+    btitle.appendChild(bauthor);
+    const removeButton = document.createElement('button');
+    removeButton.setAttribute('onclick', `removeFunction('${books[i].title}')`);
+    removeButton.id = `remove${books[i].title}`;
+    removeButton.textContent = 'remove';
     contain.appendChild(btitle);
-    contain.appendChild(bauthor);
+    contain.appendChild(removeButton);
   }
 }
 
 showBooks();
+
+// eslint-disable-next-line no-unused-vars
+function removeFunction(title) {
+  for (let i = 0; i < books.length; i += 1) {
+    if (books[i].title === title) {
+      books.splice(i, 1);
+      localStorage.setItem('savedArray', JSON.stringify(books));
+      const deleteBook = document.querySelector(`#${CSS.escape(title)}`);
+      const deleteBookButton = document.querySelector(`#remove${CSS.escape(title)}`);
+      deleteBook.parentNode.removeChild(deleteBook);
+      deleteBookButton.parentNode.removeChild(deleteBookButton);
+    }
+  }
+}
