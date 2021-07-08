ab4cb8c6329935f7becea496236782e5c4d8e61b
Erez <erezfree29@gmail.com>
2021-07-08 15:59:09 -0700

    time

--- luxon.js
+++ luxon.js
@@ -8344,9 +8344,46 @@ var luxon = (function (exports) {
 const currentTime = document.querySelector('.date');
 
 
-const dateToday = DateTime.now();
-currentTime.textContent = dateToday;
 
+
+
+
+function my_Clock() 
+  {
+  this.cur_date = new Date();
+  this.hours = this.cur_date.getHours();
+  this.minutes = this.cur_date.getMinutes();
+  this.seconds = this.cur_date.getSeconds();
+  }
+my_Clock.prototype.run = function ()
+  {
+  setInterval(this.update.bind(this), 1000);
+  };
+my_Clock.prototype.update = function () 
+  {
+  this.updateTime(1);
+  currentTime.textContent = this.hours + ":" + this.minutes + ":" + this.seconds;
+  };
+my_Clock.prototype.updateTime = function (secs) 
+  {
+  this.seconds+= secs;
+ if (this.seconds >= 60)
+  {
+  this.minutes++;
+  this.seconds= 0;
+  }
+ if (this.minutes >= 60)
+  {
+  this.hours++;
+  this.minutes=0;
+  }
+if (this.hours >= 24)
+  {
+  this.hours = 0;
+  }
+};
+var clock = new my_Clock();
+  clock.run();
   return exports;
 
 }({}));
